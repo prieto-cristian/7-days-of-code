82bb6f8e058edc89f817f5329a5fdf773b11279a
Prieto Cristian <pcristiandev@gmail.com>
2025-09-03 16:39:52 -0300

    Agregue el desafio del dia 5 al repositorio

---
+++ 05-dia/README.md
@@ -0,0 +1,61 @@
+# ¿Sabes cuando vas al supermercado con una lista de compras y terminas yendo y volviendo por los mismos pasillos hasta completar la lista?
+
+Necesitas una manzana y vas al área de frutas. El siguiente ítem es una leche y te diriges a los lácteos. Pero luego anotaste una pera, y necesitas regresar de nuevo al área de frutas.
+
+¡Después de resolver el desafío de hoy, con certeza no harás más eso!
+
+Al igual que nuestra lista de compras, es muy común que los programas trabajen con listas de string, números y objetos.
+
+Piensa en cada catálogo de e-commerce que has visto, en la lista de eventos de tu Google Calendar, o incluso en tu bandeja de entrada de correos electrónicos. Todos estos sitios utilizan listas para mostrar información de una manera simple y fácil de entender.
+
+Además, puedes aprovechar las listas para hacer filtros, ordenaciones y otras funcionalidades muy útiles.
+
+En este punto, ya debes haber notado que trabajar con estas colecciones es algo que necesitarás dominar, ¿verdad?
+
+---
+
+Entonces hoy, para facilitar tu visita al supermercado, debes crear un programa en Javascript que pregunte si deseas agregar un alimento a tu lista de compras, y debes poder responder con "sí" o "no".
+
+A continuación, preguntará qué alimento deseas agregar, y escribirás su nombre, como por ejemplo "zanahoria".
+
+Después, deberá preguntar en qué categoría se encaja ese alimento, con algunas opciones ya predefinidas, como frutas, lácteos, congelados, dulces y lo que más creas interesante. Así podrás separar todo en su respectivo grupo.
+
+Por último, en caso de que ya no quieras agregar nada más a la lista de compras y respondas "no" a la primera pregunta, se mostrará una lista con todos los ítems agrupados, de la siguiente manera:
+
+Si añades a tu lista:
+
+banana, leche en polvo, tomate, leche vegetal, chicle, gominola, manzana, uva, aguacate y leche de vaca.
+
+El programa debería imprimir, por ejemplo:
+
+Lista de compras:
+
+Frutas: banana, tomate, manzana, uva, aguacate
+
+Lácteos: leche vegetal, leche de vaca, leche en polvo
+
+Congelados:
+
+Dulces: chicle y gominola
+
+## CONSEJO
+
+Existe un objeto dentro del lenguaje Javascript que se usa justamente para crear listas de elementos, llamado Array. ¡Úsalo y abusa de él!
+
+Recuerda que siempre puedes estilizar tu programa de la manera que desees, incluso utilizando otras tecnologías para ello, como HTML y CSS.
+
+Sin embargo, eso no es obligatorio en nuestra lista de lógica de programación con Javascript. Como mencioné en los días anteriores, puedes utilizar recursos como console.log, alert y prompt para desarrollar tu programa.
+
+No olvides compartir tu código en tu GitHub y en tus redes sociales con el hashtag #7DaysOfCode.
+
+## EXTRA
+
+Para crear un array vacío, puedes usar corchetes. Y luego, para insertar algo en un array, puedes usar la función .push(). Por ejemplo:
+
+let miArray = [];
+
+miArray.push(elemento1);
+
+Después de eso, el array ya no estará vacío, tendrá el elemento 1.
+
+Para aprender más sobre arrays en Javascript, échale un vistazo a este site.
---
+++ 05-dia/desafio-5.js
@@ -0,0 +1,186 @@
+const dialogoAgregarProducto = document.getElementById("dialogoProducto");
+const campoNombreProducto = document.getElementById("campoNombre");
+const opcionSinSeleccionar = document.getElementById("opcionInicial");
+const cbxOpciones = document.getElementById('campoCategoria');
+const ulListaTodos = document.getElementById("todos");
+const ulListaEspecifica = document.getElementById("especifico");
+const tituloListado = document.querySelector(".lista__hojaDerecha__listado_titulo");
+const botonesFiltros = document.querySelectorAll(".hojaIzquierda__botonesFiltro > button");
+const resultadoOperacion = document.getElementById("resultadoOperacion");
+
+let colorTituloActual = "";
+let opcionCategoriaActual = "-1";
+let listaFrutas = ["Zanahoria"];
+let listaLacteos = ["Leche", "Yogur"];
+let listaCongelados = [];
+let listaDulces = [];
+let listaBebidas = ["Fernet", "Gancia", "Cerveza"];
+let listaOtros = [];
+let listaTodos = ["Zanahoria", "Leche", "Yogur", "Fernet", "Gancia", "Cerveza"];
+
+// funcionalidad para filtrar
+function mostrarLista(unaLista, nombreTitulo, claseColor) {
+    let ulAModificar;
+    if (nombreTitulo == "Todos") {
+        // Limpiamos la lista de todos los items
+        limpiarItemsVisuales(document.querySelectorAll("#todos > li"));
+        ulAModificar = ulListaTodos;
+        ulListaTodos.style.display = "flex";
+        ulListaEspecifica.style.display = "none";
+    } else {
+        // Limpiamos los items visuales
+        limpiarItemsVisuales(document.querySelectorAll("#especifico > li"));
+        // Ocultamos la lista general
+        ulListaTodos.style.display = "none";
+        ulListaEspecifica.style.display = "flex";
+        ulAModificar = ulListaEspecifica;
+    }
+
+    // Modificamos el titulo
+    tituloListado.textContent = nombreTitulo;
+    if (colorTituloActual != "") {
+        tituloListado.classList.remove(colorTituloActual);
+    }
+    tituloListado.classList.add(claseColor);
+    unaLista.forEach(element => {
+        let itemElemento = crearElemento(element);
+        ulAModificar.appendChild(itemElemento);
+    })
+    colorTituloActual = claseColor;
+}
+// Darle a cada boton su metodo
+botonesFiltros.forEach(element => {
+    console.log(element.textContent);
+    switch (element.textContent) {
+        case ("FRUTAS"): {
+            element.addEventListener("click", () => mostrarLista(listaFrutas, "Frutas", "color-celeste"));
+            break;
+        }
+        case ("LACTEOS"): {
+            element.addEventListener("click", () => mostrarLista(listaLacteos, "Lacteos", "color-blanco"));
+            break;
+        }
+        case ("CONGELADOS"): {
+            element.addEventListener("click", () => mostrarLista(listaCongelados, "Congelados", "color-amarillo"));
+            break;
+        }
+        case ("DULCES"): {
+            element.addEventListener("click", () => mostrarLista(listaDulces, "Dulces", "color-morado"));
+            break;
+        }
+        case ("BEBIDAS"): {
+            element.addEventListener("click", () => mostrarLista(listaBebidas, "Bebidas", "color-naranja"));
+            break;
+        }
+        case ("OTROS"): {
+            element.addEventListener("click", () => mostrarLista(listaOtros, "Otros", "color-verde"));
+            break;
+        }
+        default: {
+            element.addEventListener("click", () => mostrarLista(listaTodos, "Todos", "color-negro"));
+            break;
+        }
+    }
+});
+cbxOpciones.addEventListener("change", function () {
+    opcionCategoriaActual = this.value;
+});
+
+
+function agregarProductos() {
+    if (campoNombreProducto.value != "") {
+        if ((opcionCategoriaActual != "-1")) {
+            switch (opcionCategoriaActual) {
+                case ("frutas"): {
+                    listaFrutas.push(campoNombreProducto.value);
+                    break;
+                }
+                case ("lacteos"): {
+                    listaLacteos.push(campoNombreProducto.value);
+                    break;
+                }
+                case ("congelados"): {
+                    listaCongelados.push(campoNombreProducto.value);
+                    break;
+                }
+                case ("dulces"): {
+                    listaDulces.push(campoNombreProducto.value);
+                    break;
+                }
+                case ("bebidas"): {
+                    listaBebidas.push(campoNombreProducto.value);
+                    break;
+                }
+                default: {
+                    listaOtros.push(campoNombreProducto.value);
+                    break;
+                }
+            }
+            listaTodos.push(campoNombreProducto.value);
+            mostrarElemento(campoNombreProducto.value);
+        } else {
+            alert("Debe seleccionar una categoria", "Advertencia");
+        }
+    } else {
+        alert("Ingrese el nombre del producto", "Advertencia");
+    }
+}
+function mostrarElemento(itemTexto) {
+    // Debo crear el li
+    let itemElemento = crearElemento(itemTexto);
+    // Agregarlo al ul
+    ulListaTodos.appendChild(itemElemento);
+    // Colocar en el dialogo el texto que indica que agregamos el producto
+    resultadoOperacion.textContent = `Agregamos: ${itemTexto} a la lista de compras`;
+}
+
+function crearElemento(texto) {
+    let liElemento = document.createElement("li");
+    let labelElemento = document.createElement("label");
+    labelElemento.textContent = texto;
+
+    let inputElemento = document.createElement("input");
+    inputElemento.setAttribute("type", "checkbox");
+    
+    // Armar
+    labelElemento.appendChild(inputElemento);
+    liElemento.appendChild(labelElemento);
+
+    return liElemento;
+}
+function abrirDialogo() {
+    // Mostrar el dialogo para ingresar productos
+    dialogoAgregarProducto.style.display = "flex";
+}
+function cerrarDialogo() {
+    // Si solo agrego la propiedad selected, item vuelve al default
+    // en la primera iteracion nomas. Por eso lo saco y vuelvo a meter 
+    opcionSinSeleccionar.removeAttribute("selected");
+    opcionSinSeleccionar.setAttribute("selected", "true");
+    opcionCategoriaActual = "-1";
+
+    // Oculto el dialogo y borro el producto escrito
+    dialogoAgregarProducto.style.display = "none";
+    campoNombreProducto.value = "";
+    resultadoOperacion.textContent = "";
+}
+
+function limpiarListas() {
+    listaFrutas = [];
+    listaLacteos = [];
+    listaCongelados = [];
+    listaDulces = [];
+    listaBebidas = [];
+    listaOtros = [];
+    listaTodos = [];
+
+    let itemsListaTodos = document.querySelectorAll("#todos > li");
+    let itemsListaEspecifica = document.querySelectorAll("#especifico > li");
+    limpiarItemsVisuales(itemsListaTodos);
+    limpiarItemsVisuales(itemsListaEspecifica);
+}
+function limpiarItemsVisuales(itemsLi) {
+    itemsLi.forEach(element => {
+        element.remove();
+    });
+}
